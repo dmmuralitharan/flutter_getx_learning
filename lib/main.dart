e330d33e859dff3185a3a6164515ad43d219d88e
dmmuralitharan <dmmuralitharan@gmail.com>
2025-07-31 20:23:19 +0530

    remove: initial controller binding

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-// import 'package:ecom/app/features/product/controllers/cart_controller.dart';
 import 'package:ecom/app/routes/app_pages.dart';
 import 'package:ecom/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  // Get.put(CartController());
   runApp(const MyApp());
 }
 
